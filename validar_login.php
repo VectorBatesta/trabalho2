e3d174abb238940b3d8098bb2744977ccb7a0b7d
VectorBatesta <vitorbatista@alunos.utfpr.edu.br>
2024-11-21 17:28:00 -0300

    fixed some stuff

--- validar_login.php
+++ validar_login.php
@@ -2,7 +2,7 @@
     $usuario = $_POST["usuario"];
     $senha = $_POST["senha"];
     $query = "SELECT * FROM usuarios WHERE usuario='$usuario' and senha='$senha'";
-    $conexao = new PDO('mysql:host=127.0.0.1; dbname=bdd3', 'admi', "1234");
+    $conexao = new PDO('mysql:host=localhost; port=3307; dbname=bdd3', 'admi', "1234");
     $resultado = $conexao->query($query);
     $logado = $resultado->fetch();
 
